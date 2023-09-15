86d1e7f93032f0fe717a0973425edef9ee729f1a
ralz9 <ralz9-ralz9@mail.ru>
2023-09-15 07:22:35 +0600

    its over added by Laiĸi,Rating,CRUD,Swagger,Search by movie title,filter by name

--- applications/cinema/serializers.py
+++ applications/cinema/serializers.py
@@ -7,17 +7,25 @@ class LikeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Like
         fields = '__all__'
+
+
 class MovieSerializer(serializers.ModelSerializer):
     likes = LikeSerializer(many=True, read_only=True)
     owner = serializers.ReadOnlyField(source='owner.email')
 
+    """
+    Когда вы создаете собственный сериализатор, который наследует от serializers.ModelSerializer, как в вашем случае, 
+    вы можете переопределить метод to_representation, чтобы настроить, как объект будет сериализован в словарь (JSON-подобный объект).
+    """
     def to_representation(self, instance):
-        # print(instance)
-        rep =  super().to_representation(instance)
-        # print(rep)
-        # rep['name'] = 'John'
+        rep = super().to_representation(instance)
         rep['like_count'] = instance.likes.filter(is_like=True).count()
-
+        """
+        instance.likes - это обратная связь (related_name), которая позволяет получить все связанные объекты Like,
+         привязанные к данному instance (фильму).
+        instance в данном контексте представляет объект модели Movie, который сериализуется с дополнительными полями 
+        like_count и rating, указывающими на количество лайков и среднюю оценку для этого фильма.
+        """
         rating_result = 0
         for rating in instance.ratings.all():
             rating_result += rating.rating
